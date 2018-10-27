bb4f3bc5cd177eaefcfd18fbd2ff52c3ca217e67
flypiggg818 <qiansucheng2018@outlook.com>
2018-10-27 12:00:25 +0800

    support data structure

--- bond.py
+++ bond.py
@@ -2,11 +2,14 @@ class Bond:
 	def __init__(self):
 		pass
 
-	def trade(self):
-		trades = [{'type': 'add', 'order_id': 0, 'symbol': 'BOND', 'dir': 'BUY', 'price': 999, 'size': 10},
-						  {'type': 'add', 'order_id': 1, 'symbol': 'BOND', 'dir': 'SELL', 'price': 1001, 'size': 10}]
-		# for trade in trades:
-		# 	all_trades[trade['order_id']] = trade
+	def trade(self, order_obj, all_trades):
+		trades = [
+			{'type': 'add', 'order_id': order_obj.getOrder(), 'symbol': 'BOND', 'dir': 'BUY', 'price': 999, 'size': 85},
+			{'type': 'add', 'order_id': order_obj.getOrder(), 'symbol': 'BOND', 'dir': 'BUY', 'price': 998, 'size': 10},
+			{'type': 'add', 'order_id': order_obj.getOrder(), 'symbol': 'BOND', 'dir': 'BUY', 'price': 997, 'size': 5},
+			{'type': 'add', 'order_id': order_obj.getOrder(), 'symbol': 'BOND', 'dir': 'SELL', 'price': 1001, 'size': 85},
+			{'type': 'add', 'order_id': order_obj.getOrder(), 'symbol': 'BOND', 'dir': 'SELL', 'price': 1002, 'size': 10},
+			{'type': 'add', 'order_id': order_obj.getOrder(), 'symbol': 'BOND', 'dir': 'SELL', 'price': 1003, 'size': 5}]
 		return trades
 
 	def hello(self):
--- test-bot.py
+++ test-bot.py
@@ -45,6 +45,15 @@ def write_to_exchange(exchange, obj):
 def read_from_exchange(exchange):
 	return json.loads(exchange.readline())
 
+class Order:
+	def __init__(self):
+		self.order = 0
+
+	def getOrder(self):
+		ret = self.order
+		self.order += 1
+		return ret
+
 
 # ~~~~~============== MAIN LOOP ==============~~~~~
 
@@ -62,6 +71,7 @@ def main():
 	positions = {'BOND': 0, 'BABZ': 0, 'BABA': 0, 'AAPL': 0, 'MSFT': 0, 'GOOG': 0, 'XLK': 0}
 	book = {}
 	all_trades = {}
+	order_obj = Order()
 
 	print("The exchange replied:", hello_from_exchange, file=sys.stderr)
 	print(type(hello_from_exchange))
@@ -70,8 +80,9 @@ def main():
 
 	bond_obj = bond.Bond()
 	# bond_obj.hello()
-	trades = bond_obj.trade(all_trades)
+	trades = bond_obj.trade(order_obj, all_trades)
 	for trade in trades:
+		all_trades[trade['order_id']] = trade
 		write_to_exchange(exchange, trade)
 
 	while (1):
@@ -80,17 +91,23 @@ def main():
 			if (reply['dir'] == 'buy'):
 				positions[reply['symbol']] += reply['size']
 				cash -= reply['size'] * reply['price']
+				all_trades[reply['order_id']] -= reply['size']
+				if (all_trades[reply['order_id']] == 0):
+					all_trades.pop(reply['order_id'])
 
 			elif (reply['dir'] == 'sell'):
 				positions[reply['symbol']] -= reply['size']
 				cash += reply['size'] * reply['price']
-
+				all_trades[reply['order_id']] -= reply['size']
+				if (all_trades[reply['order_id']] == 0):
+					all_trades.pop(reply['order_id'])
 			print("log, fill-reply received: ", reply)
 
 		print(reply)
 		if (reply['type'] == 'out'):
 			trades = bond_obj.trade()
 			for trade in trades:
+				all_trades[trade['order_id']] = trade
 				write_to_exchange(exchange, trade)
 
 if __name__ == "__main__":
