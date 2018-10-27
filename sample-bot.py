4f8fe798adf08c8248eb96ec27239aeec57ff56b
flypiggg818 <qiansucheng2018@outlook.com>
2018-10-27 13:05:06 +0800

    lala

--- sample-bot.py
+++ sample-bot.py
@@ -11,13 +11,14 @@ import sys
 import socket
 import json
 import bond
+import baaz
 
 # ~~~~~============== CONFIGURATION  ==============~~~~~
 # replace REPLACEME with your team name!
 team_name = "Furret"
 # This variable dictates whether or not the bot is connecting to the prod
 # or test exchange. Be careful with this switch!
-test_mode = False
+test_mode = True
 
 # This setting changes which test exchange is connected to.
 # 0 is prod-like
@@ -45,6 +46,15 @@ def write_to_exchange(exchange, obj):
 def read_from_exchange(exchange):
 	return json.loads(exchange.readline())
 
+class Order:
+	def __init__(self):
+		self.order = 0
+
+	def getOrder(self):
+		ret = self.order
+		self.order += 1
+		return ret
+
 
 # ~~~~~============== MAIN LOOP ==============~~~~~
 
@@ -62,6 +72,7 @@ def main():
 	positions = {'BOND': 0, 'BABZ': 0, 'BABA': 0, 'AAPL': 0, 'MSFT': 0, 'GOOG': 0, 'XLK': 0}
 	book = {}
 	all_trades = {}
+	order_obj = Order()
 
 	print("The exchange replied:", hello_from_exchange, file=sys.stderr)
 	print(type(hello_from_exchange))
@@ -69,29 +80,51 @@ def main():
 	positions = hello_from_exchange["symbols"]
 
 	bond_obj = bond.Bond()
+
 	# bond_obj.hello()
-	trades = bond_obj.trade(all_trades)
+	trades = bond_obj.trade(order_obj, all_trades)
 	for trade in trades:
+		all_trades[trade['order_id']] = trade
+		write_to_exchange(exchange, trade)
+
+	baaz_obj = baaz.Baaz()
+	trades = baaz_obj.trade(book, order_obj, positions, all_trades)
+	for trade in trades:
+		all_trades[trade['order_id']] = trade
 		write_to_exchange(exchange, trade)
 
 	while (1):
 		reply = read_from_exchange(exchange)
+		if (reply['type'] == 'book'):
+			# print("book: ", reply)
+			book[reply['symbol']] = {'buy': reply['buy'], 'sell': reply['sell']}
+			trades = bond_obj.trade(order_obj, all_trades)
+			for trade in trades:
+				all_trades[trade['order_id']] = trade
+				write_to_exchange(exchange, trade)
+
 		if (reply['type'] == 'fill'):
 			if (reply['dir'] == 'buy'):
 				positions[reply['symbol']] += reply['size']
 				cash -= reply['size'] * reply['price']
+				all_trades[reply['order_id']] -= reply['size']
+				if (all_trades[reply['order_id']] == 0):
+					all_trades.pop(reply['order_id'])
 
 			elif (reply['dir'] == 'sell'):
 				positions[reply['symbol']] -= reply['size']
 				cash += reply['size'] * reply['price']
-
+				all_trades[reply['order_id']] -= reply['size']
+				if (all_trades[reply['order_id']] == 0):
+					all_trades.pop(reply['order_id'])
 			print("log, fill-reply received: ", reply)
 
-		print(reply)
 		if (reply['type'] == 'out'):
-			trades = bond_obj.trade()
+			trades = bond_obj.trade(order_obj, all_trades)
 			for trade in trades:
+				all_trades[trade['order_id']] = trade
 				write_to_exchange(exchange, trade)
+
 
 if __name__ == "__main__":
 	main()
--- test-bot.py
+++ test-bot.py
@@ -11,13 +11,14 @@ import sys
 import socket
 import json
 import bond
+import baaz
 
 # ~~~~~============== CONFIGURATION  ==============~~~~~
 # replace REPLACEME with your team name!
 team_name = "Furret"
 # This variable dictates whether or not the bot is connecting to the prod
 # or test exchange. Be careful with this switch!
-test_mode = False
+test_mode = True
 
 # This setting changes which test exchange is connected to.
 # 0 is prod-like
@@ -79,14 +80,25 @@ def main():
 	positions = hello_from_exchange["symbols"]
 
 	bond_obj = bond.Bond()
+
 	# bond_obj.hello()
 	trades = bond_obj.trade(order_obj, all_trades)
 	for trade in trades:
 		all_trades[trade['order_id']] = trade
 		write_to_exchange(exchange, trade)
 
+	baaz_obj = baaz.Baaz()
+	trades = baaz_obj.trade(book, order_obj, positions, all_trades)
+	for trade in trades:
+		all_trades[trade['order_id']] = trade
+		write_to_exchange(exchange, trade)
+
 	while (1):
 		reply = read_from_exchange(exchange)
+		if (reply['type'] == 'book'):
+			# print("book: ", reply)
+			book[reply['symbol']] = {'buy': reply['buy'], 'sell': reply['sell']}
+
 		if (reply['type'] == 'fill'):
 			if (reply['dir'] == 'buy'):
 				positions[reply['symbol']] += reply['size']
@@ -103,9 +115,8 @@ def main():
 					all_trades.pop(reply['order_id'])
 			print("log, fill-reply received: ", reply)
 
-		print(reply)
 		if (reply['type'] == 'out'):
-			trades = bond_obj.trade()
+			trades = bond_obj.trade(order_obj, all_trades)
 			for trade in trades:
 				all_trades[trade['order_id']] = trade
 				write_to_exchange(exchange, trade)
